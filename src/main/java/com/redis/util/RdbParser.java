cce36a09e827f203f555cfa9f6ce5376d712f134
litesam <lightsam126@gmail.com>
2025-08-27 21:37:54 +0530

    fix: RDB parser

--- src/main/java/com/redis/util/RdbParser.java
+++ src/main/java/com/redis/util/RdbParser.java
@@ -17,9 +17,6 @@ public class RdbParser {
 
     public void load() throws IOException {
         // Header section
-        // Metadata section
-        // Database section
-        // End of file section
         parseHeader();
 
         Integer databaseNumber = 0;
@@ -27,7 +24,7 @@ public class RdbParser {
         while (true) {
             final var opcode = inputStream.readNBytes(1)[0];
             if (opcode == RdbConstants.END_OF_FILE) {
-                System.out.println("Reached end of file for rdb");
+                System.out.println("Reached end of file for RDB");
                 break;
             }
 
@@ -35,25 +32,28 @@ public class RdbParser {
                 case RdbConstants.METADATA_SECTION -> {
                     final var key = readString();
                     final var value = readString();
-                    System.out.printf("metadata: %s, %s\n", key, value);
+                    System.out.printf("Metadata: %s, %s\n", key, value);
                 }
                 case RdbConstants.DATABASE_START -> {
                     databaseNumber = readUnsignedByte();
-                    System.out.printf("databaseNumber: %d\n", databaseNumber);
+                    System.out.printf("Database Number: %d\n", databaseNumber);
                 }
                 case RdbConstants.HASH_TABLE_SIZE -> {
                     final var hashTableSize = readLength();
-                    System.out.printf("hashTableSize: %d\n", hashTableSize);
+                    System.out.printf("Hash Table Size: %d\n", hashTableSize);
 
                     final var expireHashTableSize = readLength();
-                    System.out.printf("expireHashTableSize: %d\n", expireHashTableSize);
+                    System.out.printf("Expire Hash Table Size: %d\n", expireHashTableSize);
                 }
                 default -> {
                     if (databaseNumber == null) {
-                        //?
+                        throw new RuntimeException("Database number is null");
                     }
+
                     int valueType = opcode;
                     Long expiration = null;
+
+                    // Check if the key has an expiration timestamp
                     if (opcode == RdbConstants.P_EXPIRE_T) {
                         expiration = readUnsignedLong();
                         valueType = readUnsignedByte();
@@ -61,7 +61,8 @@ public class RdbParser {
 
                     final var key = readString();
                     final var value = readValue(valueType);
-                    //TODO: Fill DataStore
+
+                    // Store the key-value pair with expiration in the DataStore
                     dataStore.set(key, value, expiration);
                 }
             }
@@ -71,7 +72,7 @@ public class RdbParser {
     private String readValue(int valueType) throws IOException {
         return switch (valueType) {
             case RdbConstants.STRING_VALUE_BYTE -> readString();
-            default -> throw new RuntimeException("unsupported value type: " + valueType);
+            default -> throw new RuntimeException("Unsupported value type: " + valueType);
         };
     }
 
@@ -80,11 +81,11 @@ public class RdbParser {
 
         if (length < 0) {
             final var type = (-length) - 1;
-            return switch(type) {
+            return switch (type) {
                 case RdbConstants.STRING_INTEGER_8BIT -> String.valueOf(Byte.toUnsignedInt(inputStream.readNBytes(1)[0]));
-                case RdbConstants.STRING_INTEGER_16BIT -> String.valueOf(Short.toUnsignedInt(Short.reverseBytes(inputStream.readNBytes(2)[0])));
-                case RdbConstants.STRING_INTEGER_32BIT -> Integer.toUnsignedString(Integer.reverseBytes(inputStream.readNBytes(4)[0]));
-                default -> throw new RuntimeException("No type found");
+                case RdbConstants.STRING_INTEGER_16BIT -> String.valueOf(Short.toUnsignedInt(Short.reverseBytes(readShort())));
+                case RdbConstants.STRING_INTEGER_32BIT -> String.valueOf(Integer.toUnsignedLong(Integer.reverseBytes(readInt())));
+                default -> throw new RuntimeException("Unsupported string type: " + type);
             };
         }
 
@@ -97,11 +98,11 @@ public class RdbParser {
     }
 
     public long readUnsignedInteger() throws IOException {
-        return Integer.toUnsignedLong(Integer.reverseBytes(inputStream.readNBytes(4)[0]));
+        return Integer.toUnsignedLong(Integer.reverseBytes(readInt()));
     }
 
     public long readUnsignedLong() throws IOException {
-        return Long.reverseBytes(inputStream.readNBytes(8)[0]);
+        return Long.reverseBytes(readLong());
     }
 
     public int readLength() throws IOException {
@@ -117,14 +118,14 @@ public class RdbParser {
                 yield (value << 8) | second;
             }
             case RdbConstants.LENGTH_SPECIAL -> -(value + 1);
-            default -> throw new RuntimeException("no value found");
+            default -> throw new RuntimeException("Invalid length encoding");
         };
     }
 
     private void parseHeader() throws IOException {
         final var header = new String(inputStream.readNBytes(5));
 
-        if (!header.equals(("REDIS"))) {
+        if (!header.equals("REDIS")) {
             throw new RuntimeException("Invalid file format");
         }
 
@@ -135,5 +136,27 @@ public class RdbParser {
     private int parseVersion() throws IOException {
         final var version = new String(inputStream.readNBytes(4));
         return Integer.parseInt(version);
+    }
+
+    private short readShort() throws IOException {
+        final var bytes = inputStream.readNBytes(2);
+        return (short) ((bytes[1] << 8) | (bytes[0] & 0xFF));
+    }
+
+    private int readInt() throws IOException {
+        final var bytes = inputStream.readNBytes(4);
+        return (bytes[3] << 24) | ((bytes[2] & 0xFF) << 16) | ((bytes[1] & 0xFF) << 8) | (bytes[0] & 0xFF);
+    }
+
+    private long readLong() throws IOException {
+        final var bytes = inputStream.readNBytes(8);
+        return ((long) bytes[7] << 56) |
+                ((long) (bytes[6] & 0xFF) << 48) |
+                ((long) (bytes[5] & 0xFF) << 40) |
+                ((long) (bytes[4] & 0xFF) << 32) |
+                ((long) (bytes[3] & 0xFF) << 24) |
+                ((long) (bytes[2] & 0xFF) << 16) |
+                ((long) (bytes[1] & 0xFF) << 8) |
+                ((long) (bytes[0] & 0xFF));
     }
 }
